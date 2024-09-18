34ab0a27ca09a229dc6ae0ecc2d8cf70dc155e83
Markus Pöschl <Poeschl@users.noreply.github.com>
2024-09-18 16:01:33 +0000

    ♻️ Allow image copy script to accept a list of images

--- scripts/transfer-addon-image-2-registry.sh
+++ scripts/transfer-addon-image-2-registry.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
 set -e
 
-SOURCE_IMAGE=$1
-TARGET_REGISTRY=$2
+TARGET_REGISTRY=$1
+SOURCE_IMAGES=(${*: 2})
+DRY_RUN=1
 
-if [[ -z "$SOURCE_IMAGE" ]]; then
-  echo 'No source image specified. Needs to be written as "transfer-addon-image-2-registry.sh <source-image> <target-registry>."'
-  echo 'Example: ./transfer-addon-image-2-registry.sh ghcr.io/poeschl/ha-icantbelieveitsnotvaletudo-amd64:4.0.0 ghcr.io/poeschl-homeassistant-addons'
+if [[ -z "$SOURCE_IMAGES" ]]; then
+  echo 'No source image specified. Needs to be written as "transfer-addon-image-2-registry.sh <target-registry> <source-images> <source-image> ..."'
+  echo 'Example: ./transfer-addon-image-2-registry.sh ghcr.io/poeschl-homeassistant-addons ghcr.io/poeschl/ha-icantbelieveitsnotvaletudo-amd64:4.0.0'
   echo 'You need to be login-ed to the registries already!'
   exit 1
 fi
 
-IMAGE_WITHOUT_REGISTRY=$( cut -d '/' -f 2- <<< "$SOURCE_IMAGE" )
-IMAGE_WITH_NEW_REGISTRY="${TARGET_REGISTRY}/${IMAGE_WITHOUT_REGISTRY}"
+for image in "${SOURCE_IMAGES[@]}"
+do
+  IMAGE_WITHOUT_REGISTRY=$( cut -d '/' -f 3- <<< "$image" )
+  IMAGE_WITHOUT_REGISTRY=${IMAGE_WITHOUT_REGISTRY#'ha-'}
+  IMAGE_WITH_NEW_REGISTRY="${TARGET_REGISTRY}/${IMAGE_WITHOUT_REGISTRY}"
 
-echo "Transfer $SOURCE_IMAGE -> $IMAGE_WITH_NEW_REGISTRY"
+  echo "Transfer $image -> $IMAGE_WITH_NEW_REGISTRY"
 
-podman pull "$SOURCE_IMAGE"
-podman tag "$SOURCE_IMAGE" "$IMAGE_WITH_NEW_REGISTRY"
-podman push "$IMAGE_WITH_NEW_REGISTRY"
+  if [ "$DRY_RUN" = "0" ]; then
+    podman pull "$image"
+    podman tag "$image" "$IMAGE_WITH_NEW_REGISTRY"
+    podman push "$IMAGE_WITH_NEW_REGISTRY"
 
-podman rmi "$SOURCE_IMAGE" "$IMAGE_WITH_NEW_REGISTRY"
+    podman rmi "$image" "$IMAGE_WITH_NEW_REGISTRY"
+  else
+    echo "!!! Dry run !!!"
+  fi
+done
